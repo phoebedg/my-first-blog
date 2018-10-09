925bce0cbf48dec0022b7500c429e831f65dddf3
Phoebe Gash <phoebe.gash@verve.co>
2018-10-09 20:49:35 +0100

    turned debug to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*-0dlfpfc_ij8!rbbws49w&be8_4#2g&@h_1$xa8^cx(ktpgwg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
